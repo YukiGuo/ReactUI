435505f1a2eee8d031cb8cf0d8c06ea722b848ed
yukiguo <83451796@qq.com>
2023-03-19 11:47:55 +0800

    测试覆盖率

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -4,7 +4,10 @@ import Icon from '../Icon'
 
 describe('icon', () => {
     it('是个 svg', () => {
+        console.log('hhhsvg1111')
         const json = renderer.create(<Icon name="add" />).toJSON()
+        console.log('hhhsvg')
         expect(json).toMatchSnapshot()
+        console.log('hhhsvg2')
     })
 })
