379ffd61ebf1675497c7ea9d81b90136aabcfcd0
郭小梅 <sally.guo@ersoft.cn>
2023-03-28 11:51:52 +0800

    feat: button props类型指定

--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -3,15 +3,15 @@ import './button.scss';
 import mergeClassName from "../tools/mergeClassName";
 
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
-    type?: string;
-    size?: string;
+    type?: 'default' | 'primary' | 'text'|'success'|'warning';
+    size?: 'middle' | 'small' | 'large';
     loading?: string;
     // icon: ReactNode;
     disabled?: boolean;
 }
-const Button: React.FunctionComponent<ButtonProps> = ({ type = "default", size = "middler", loading, disabled, children }) => {
+const Button: React.FunctionComponent<ButtonProps> = ({ type = "default", size = "middler", loading, disabled, children ,...restProps }) => {
 
-    return <button className={mergeClassName('koala-button', `koala-${type}-button`, `koala-${size}-button`)}>
+    return <button className={mergeClassName('koala-button', `koala-${type}-button`, `koala-${size}-button`)} {...restProps} >
         {children}
     </button>
 
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,3 @@
 export { default as Icon } from './icon/Icon';
+export { default as Button } from './button/button';
+export { default as Dialog ,alert,confirm}  from './dialog/Dialog';
