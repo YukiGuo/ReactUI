548efafb9c5cc15fa070b83847c4a86d4d6c259c
郭小梅 <sally.guo@ersoft.cn>
2023-04-13 15:09:53 +0800

    fix: 更改配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,10 +4,10 @@ const repo = 'ReactUI'; // 项目名
 
 export default defineConfig({
   title: 'ReactUI',
-  mode: 'site',
-  devServer: {
-    port: 1998, // 自定义端口号
-  },
+  // mode: 'site',
+  // devServer: {
+  //   port: 1998, // 自定义端口号
+  // },
   base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 
