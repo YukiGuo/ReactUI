3c4ec8e6443588a5a1796193c2419a1e94b961a9
郭小梅 <sally.guo@ersoft.cn>
2023-04-13 14:42:27 +0800

    feat: github配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'ReactUI'; // 项目名
+
 export default defineConfig({
-  outputPath: 'docs-dist',
-  themeConfig: {
-    name: 'koala-ui',
+  title: 'ReactUI',
+  mode: 'site',
+  devServer: {
+    port: 1998, // 自定义端口号
   },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+
+  // more config: https://d.umijs.org/config
 });
